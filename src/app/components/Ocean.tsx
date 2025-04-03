f4aa43586608c8f36c3088696d731caed933d273
Adam <adam-hamid.salah-salah.1@ens.etsmtl.ca>
2025-04-03 19:40:00 -0400

    fixed resizing

--- src/app/components/Ocean.tsx
+++ src/app/components/Ocean.tsx
@@ -24,25 +24,21 @@ export default function ocean() {
         canvas.width = dimensions.width;
         canvas.height = dimensions.height;
 
-        context.font = fontSize + 'px monospace';
-        // context.fillStyle = 'oklch(0.81 0.185 208.59)';
-        context.fillStyle = '#FFFFFF';
-
-        let mapHeight: number = canvas.height / tileHeight;
+        let mapHeight: number = Math.floor(canvas.height / tileHeight);
         let currentHeight: number = map.length;
 
-        if (currentHeight < mapHeight) {
+        if (currentHeight <= mapHeight) {
             for (let i = currentHeight; i < mapHeight; i++) {
                 map.push([]);
             }
         } else {
-            setMap(map.slice(0, -(mapHeight - currentHeight)));
+            map.splice(mapHeight - currentHeight);
         }
 
-        let mapWidth: number = canvas.width / tileWidth;
+        let mapWidth: number = Math.floor(canvas.width / tileWidth);
         let currentWidth: number = map[0].length;
 
-        if (currentWidth < mapWidth) {
+        if (currentWidth <= mapWidth) {
             for (let i = 0; i < mapHeight; i++) {
                 for (let j = currentWidth; j < mapWidth; j++) {
                     map[i].push(0);
@@ -50,11 +46,12 @@ export default function ocean() {
             }
         } else {
             for (let i = 0; i < mapHeight; i++) {
-                map[i] = map[i].slice(0, -(mapWidth - currentWidth));
+                map[i].splice(mapWidth - currentWidth);
             }
         }
-        console.log(map)
     }, [dimensions]);
+
+    //the one and only canvas rendering
 
     useEffect(() => {
         if (!canvas) return;
@@ -62,6 +59,11 @@ export default function ocean() {
         if (!context) return;
         if (!dimensions) return;
 
+        context.font = fontSize + 'px monospace';
+        // context.fillStyle = 'oklch(0.81 0.185 208.59)';
+        context.fillStyle = '#FFFFFF';
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
         for (let i = 0; i < map.length; i++) {
             let line = "";
             for (let j = 0; j < map[i].length; j++) {
@@ -69,8 +71,7 @@ export default function ocean() {
             }
             context.fillText(line, 10, 50 + i * tileHeight);
         }
-        // console.log()
-    }, [dimensions]);
+    });
 
     return <canvas ref={canvasRef} id='ocean' className='h-screen' />;
 }
--- src/app/hooks/useWindowDimensions.tsx
+++ src/app/hooks/useWindowDimensions.tsx
@@ -5,19 +5,33 @@
 import { useEffect, useState } from 'react';
 import { Dimensions } from '../types';
 
-
 export default function useWindowDimensions() {
     const [windowDimensions, setWindowDimensions] = useState<Dimensions>();
+    const [canUpdate, setCanUpdate] = useState<boolean>(false);
+    const [lastResizeTime, setLastResizeTime] = useState<number>(0);
 
     useEffect(() => {
+        const interval = setInterval(() => {
+            if (canUpdate && Date.now() - lastResizeTime > 100) {
+                setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
+                setCanUpdate(false);
+            }
+        }, 100);
+
         const handleResize = () => {
-            setWindowDimensions({width: window.innerWidth, height: window.innerHeight});
+            setCanUpdate(true);
+            setLastResizeTime(Date.now());
         };
 
-        handleResize();
-
         window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            clearInterval(interval);
+        };
+    }, [canUpdate, lastResizeTime]);
+
+    useEffect(() => {
+        setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
     }, []);
 
     return windowDimensions;
