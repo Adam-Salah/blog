bb19730479a1cb315686cd7c70c11c39006339b4
Adam Hamid Salah Salah <adam-hamid.salah-salah.1@ens.etsmtl.ca>
2025-07-18 16:08:15 -0400

    fixed production bugs

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { Project } from "@/interfaces";
 import dynamic from "next/dynamic";
 import { projects } from '@/data/projects';
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
     const MDX = dynamic(() => import(`@/content/projects/${slug}.mdx`));
     return <MDX />;
